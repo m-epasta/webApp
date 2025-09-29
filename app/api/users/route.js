75de3bcc26cd24d0b019a2f187690dfc0ccfd338
m-e pasta <mepastacc@gmail.com>
2025-09-29 21:16:34 +0200

    fixed unreachable code in app/api/users/route.js

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -9,20 +9,17 @@ export async function GET(request) {
 
     if (!authHeader) {
     return new Response(JSON.stringify({ error: 'Authorization header is missing.'})), { status: 401 };
-    return isauthValid === false;
 }
     if (!process.env.DB_ENCRYPTION_KEY) {
         return new Response(JSON.stringify({ error: 'Database encryption key is not set.'})), { status: 500 };
-        return isauthValid === false;
+
         }
     if (process.env.ENABLE_DATABASE !== 'true') {
         return new Response(JSON.stringify({ message : 'Database is disabled.'})), { status: 200 };
-        return isauthValid === false;
     } else {
         const db = initDatabase();
         if (!db) {
             return new Response(JSON.stringify({ error : 'Failed to initialize database.'})), { status: 500 };
-            return isauthValid === false;
         }
         try {
             const rows = db.prepare('SELECT * FROM users').all();
@@ -30,10 +27,8 @@ export async function GET(request) {
         } catch (error) {
             console.error('Database query error:', error);
             return new Response(JSON.stringify({ error: 'Database query failed.'})), { status: 500}
-            return isauthValid === false;
         }
     }
-    return isauthValid === true;
     }
 
     
